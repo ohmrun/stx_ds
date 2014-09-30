c9bc2a5c574dae760ca84f59f68eda424c0af53f
0b1kn00b <polysemantic@gmail.com>
2014-09-30 12:32:50 +0200

    pulling horns in

--- Main.hx
+++ Main.hx
@@ -1,22 +1,31 @@
 package ;
 
-import coll.Dlist;
-import neko.Lib;
+import hx.ds.ifs.Capacity;
+import hx.ds.ifs.Data;
+import hx.ds.ifs.Indexed;
+import hx.ds.ifs.LocatableByValue;
 
-/**
- * ...
- * @author 0b1kn00b
- */
+import stx.ds.List;
+import stx.ds.ifs.Collection;
+import stx.ds.ifs.Enumerable;
+import stx.ds.ifs.Enumerator;
+import stx.ds.ifs.Filterable;
+
+import stx.ds.ifs.Insertable;
+import stx.ds.ifs.Joinable;
+
+import stx.ds.ifs.Opaque;
+import stx.ds.ifs.Place;
+import stx.ds.ifs.Searchable;
+
+import hx.ds.Array2d;
+import hx.ds.BinarySearchTree;
 
 class Main {
-	
 	static function main() {
+    trace('hello');
 		//Collection;
-		//Enumerable;
-		var q = new coll.Stack();
-		q.init( [2, 32, 345, 6, 68, 9] );
-		
+		//Enumerable;	
 		//var d = new Dlist();
 	}
-	
 }
